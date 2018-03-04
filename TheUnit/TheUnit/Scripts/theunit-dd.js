1d841f96c40a754bf592241f12afc0fc7dea169b
Ben Staley <bs16aat@herts.ac.uk>
2018-03-04 13:38:45 +0000

    Baisc Homepage Template Completed

---
+++ TheUnit/TheUnit/Scripts/theunit-dd.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#account-btn').click(function (e) {
+        $(this).toggleClass('active');
+        $('#account').toggleClass('active');
+    });
+
+    $('#search-btn').click(function (e) {
+        $(this).toggleClass('active');
+        $('#search').toggleClass('active');
+    });
+});
+
+
